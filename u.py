0f8f73409461efc9c2c2ea4b55a5ede44d666f4b
193HW2 <kevinkw.choe@gmail.com>
2020-12-28 12:10:26 -0800

    first commit

--- u.py
+++ u.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 path = "C:/Users/kevin/Documents/ds_salary_proj/chromedriver"
 
-df = gs.get_jobs('data scientist',15, False, path, 15)
+df = gs.get_jobs('data scientist',1000, False, path, 15)
 
 df.to_csv('glassdoor_jobs.csv', index = False)
